84420b2d067ec4901e27393d4dc3b3b9eb10b653
Shidhin Varghese Philip <ShidhinVarghesePhilip98@gmail.com>
2025-10-24 23:28:18 -0500

    Added Update

--- profile/lib/pages/homePage.dart
+++ profile/lib/pages/homePage.dart
@@ -1,4 +1,6 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:profile/pages/about_me.dart';
 import 'package:profile/widgets/get_height_width.dart';
 import 'package:rive/rive.dart';
 
@@ -15,7 +17,10 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       extendBodyBehindAppBar: true,
-      appBar: AppBar(elevation: 0, backgroundColor: Colors.transparent),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
       body: Stack(
         children: [
           RiveAnimation.asset(
@@ -31,17 +36,31 @@ class _HomePageState extends State<HomePage> {
             }),
           ),
           Positioned(
-            top: getHeight(context)/6,
-            left: getWidth(context)/5,
+            top: getHeight(context) / 6,
+            left: getWidth(context) / 7,
             child: Row(
               children: [
-                RichText(
-                  text: TextSpan(
-                    children: [
-                      TextSpan(text: "Hi "),
-                      TextSpan(text: "Hello Testing testing testing"),
-                    ],
-                  ),
+                AnimatedTextKit(
+                  repeatForever: true,
+                  animatedTexts: [
+                    ColorizeAnimatedText(
+                      "Hello,\n",
+                      textStyle: TextStyle(fontSize: 40),
+                      colors: [Colors.amberAccent,Colors.cyanAccent, Colors.deepOrangeAccent,Colors.indigo, Colors.amberAccent],
+                    ),
+                  ],
+                ),
+                Text("I am",style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                SizedBox(width: 10),
+                AnimatedTextKit(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context)=>AboutMePage())),
+                  animatedTexts: [
+                    ScrambleAnimatedText(
+                      "Shidhin Varghese Philip",
+                      textStyle: TextStyle(fontSize: 50),
+                      speed: Duration(milliseconds: 150),
+                    ),
+                  ],
                 ),
               ],
             ),
